8818b495a8da95d30eb20a8c95b9314030373773
Santiramoli <sanolydani@gmail.com>
2024-09-30 10:21:51 +0100

    Capture of buttons and reset of the panel.

--- script.js
+++ script.js
@@ -1,16 +1,15 @@
 const buttons = document.querySelectorAll('.buttons button');
 buttons.forEach(button => {
     button.addEventListener('click', (event) => {
-        // Obtenemos el texto del botón presionado
+
         const buttonValue = event.target.innerText;
-        
-        // Mostramos el valor del botón en la consola
-        console.log('Botón presionado:', buttonValue);
         
         if(buttonValue == "AC"){
             document.getElementById("op").innerHTML = "";
             document.getElementById("res").innerHTML = "";
         }
-        // Aquí puedes agregar lógica adicional para manejar las operaciones de la calculadora
+
+        
+
     });
 });
